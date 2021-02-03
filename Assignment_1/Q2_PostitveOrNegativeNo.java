9760a3ca3fb5b938952b6abfe1eeaf9c127a9668
Sanjit_Shelke <sanjitshelke@gmail.com>
2021-02-03 23:39:01 +0530

    Assignment_1

---
+++ Assignment_1/Q2_PostitveOrNegativeNo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q2_PostitveOrNegativeNo {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a Number : ");
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        scanner.close();
+
+        if(num>0){
+            System.out.println("positive number");
+        }
+        else{
+            System.out.println("negative number");
+        }
+    }
+}
---
+++ Assignment_1/Q5_FibonacciUptoNNo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q5_FibonacciUptoNNo {
+    
+    public static void main(String[] args) {
+        System.out.println("Enter a Number : ");
+        Scanner scanner=new Scanner(System.in);
+        int num=scanner.nextInt();
+        scanner.close();
+        int a=0;
+        int b=1;
+        
+       
+        while(a <= num){
+          System.out.println(a);
+
+          int sum= a+b;
+          a=b;
+          b=sum;
+    
+        }
+       
+    }
+}
